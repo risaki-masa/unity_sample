8a78d0b2a2bc47e48e570b9e7569aa6036e69e1b
Risaki <kyozyu_masa@yahoo.co.jp>
2021-10-07 14:04:29 +0900

    各スクリプトを追加

---
+++ Scripts/ScrollableImage.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// スクロールできる画像を管理するクラス
+/// </summary>
+public sealed class ScrollableImage : MonoBehaviour
+{
+    [SerializeField]
+    private RawImage m_image = null;
+
+    [SerializeField, Range( 0.0f, 3.0f )]
+    private float m_speed = 0.0f;
+
+    /// <summary>
+    /// 規定値に戻す
+    /// </summary>
+    private void Reset() 
+    {
+        m_image = GetComponent<RawImage>();
+    }
+
+    /// <summary>
+    /// 更新する時の処理
+    /// </summary>
+    private void Update()
+    {
+        var uv_rect     = m_image.uvRect;
+        uv_rect.x       = Mathf.Repeat( Time.time * m_speed, 1.0f );
+        m_image.uvRect  = uv_rect;
+    }
+}
---
+++ Scripts/TranslationRepeater.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 平行移動を繰り返すクラス
+/// </summary>
+public sealed class TranslationRepeater : MonoBehaviour
+{
+    [SerializeField]
+    private float start_position    = 0.0f;
+
+    [SerializeField]
+    private float end_position      = 0.0f;
+    
+    [SerializeField]
+    private float m_speed           = 0.0f;
+
+    /// <summary>
+    /// 更新する時の処理
+    /// </summary>
+    private void Update()
+    {
+        transform.Translate( new Vector3( m_speed, 0.0f, 0.0f ) );
+
+        var position = transform.localPosition;
+        if ( position.x > end_position ) return;
+
+        position.x              = start_position;
+        transform.localPosition = position;
+    }
+}
